71d67b30081926fa2881ad5012ea95dc598f84e3
solarch <joe@durangoitarchitects.com>
2015-07-11 19:07:02 -0700

    initial commit to remote repo

---
+++ LogIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LogIn
+//
+//  Created by Joe Gaber on 6/26/15.
+//  Copyright (c) 2015 durangoitarchitects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
